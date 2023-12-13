1e936fbcf513d0b4bbe51d5cfdac0d816ce642a6
amll <jiewangcn@outlook.com>
2023-12-13 20:52:29 +0800

    Day 1 - Part One

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,24 @@
+fun addFirstDigitAndLastDigit(line: String): Int {
+    var digits = ""
+    for (c in line) {
+        if (c.isDigit()) {
+            digits += c
+        }
+    }
+    val result: Int = 10 * (digits[0] - '0') + (digits[digits.length - 1] - '0')
+    println(result)
+    return result
+}
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var ret = 0
+        for (line in input) {
+            println(line)
+            ret += addFirstDigitAndLastDigit(line)
+        }
+        println(ret)
+        return ret
     }
 
     fun part2(input: List<String>): Int {
@@ -9,9 +27,10 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    part1(testInput)
+    check(part1(testInput) == 142)
 
     val input = readInput("Day01")
     part1(input).println()
-    part2(input).println()
+//    part2(input).println()
 }
