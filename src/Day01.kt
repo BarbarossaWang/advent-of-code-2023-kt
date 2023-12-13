5b43e5b6dce5e7fe6c5b5bfa35a95090f6b07ba2
amll <jiewangcn@outlook.com>
2023-12-13 22:26:35 +0800

    Day 1 - Part Two

--- src/Day01.kt
+++ src/Day01.kt
@@ -10,6 +10,32 @@ fun addFirstDigitAndLastDigit(line: String): Int {
     return result
 }
 
+fun addFirstDigitAndLastDigitWithSpellOut(line: String): Int {
+    val letters = arrayOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+    var digits = ""
+    var currentCharIndex = 0
+    while (currentCharIndex < line.length) {
+        letters.forEachIndexed { index, s ->
+            run {
+                if (currentCharIndex + s.length <= line.length &&
+                    line.substring(currentCharIndex, currentCharIndex + s.length) == s
+                ) {
+                    val newChar = "${1 + index}"
+                    digits += newChar
+                }
+            }
+        }
+        if (line[currentCharIndex].isDigit()) {
+            digits += line[currentCharIndex]
+        }
+        currentCharIndex += 1
+    }
+    val result: Int = 10 * (digits[0] - '0') + (digits[digits.length - 1] - '0')
+    println(result)
+    return result
+}
+
+
 fun main() {
     fun part1(input: List<String>): Int {
         var ret = 0
@@ -22,15 +48,21 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var ret = 0
+        for (line in input) {
+            println(line)
+            ret += addFirstDigitAndLastDigitWithSpellOut(line)
+        }
+        println(ret)
+        return ret
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    part1(testInput)
-    check(part1(testInput) == 142)
+    part2(testInput)
+    check(part2(testInput) == 281)
 
     val input = readInput("Day01")
     part1(input).println()
-//    part2(input).println()
+    part2(input).println()
 }
