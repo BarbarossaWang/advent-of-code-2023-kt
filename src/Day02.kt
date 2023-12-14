3d218fad2e2d5066b75952de6dc9beaf2829771d
amll <jiewangcn@outlook.com>
2023-12-14 11:53:45 +0800

    Day 2- Part Two

--- src/Day02.kt
+++ src/Day02.kt
@@ -49,6 +49,45 @@ fun main() {
         return isPossible
     }
 
+    fun findFewestCubeNumbers(line: String): MutableMap<String, Int> {
+        val fewestCubeNumbers = mutableMapOf("red" to 0, "green" to 0, "blue" to 0)
+
+        val splitList = line.split(":")
+        val cubeSets = splitList[1].split(";")
+        for (cubeSet in cubeSets) {
+            val cubeNumberColornames = cubeSet.split(",")
+            for (cubeNumberColorname in cubeNumberColornames) {
+                val splitCubeNumberColorname = cubeNumberColorname.trim().split(" ")
+                val cubeColorNumber = splitCubeNumberColorname[0].toInt()
+                val cubeColorName = splitCubeNumberColorname[1]
+
+                when (cubeColorName) {
+                    "red" -> {
+                        if (cubeColorNumber > fewestCubeNumbers["red"]!!) {
+                            fewestCubeNumbers["red"] = cubeColorNumber
+                        }
+                    }
+                    "green" -> {
+                        if (cubeColorNumber > fewestCubeNumbers["green"]!!) {
+                            fewestCubeNumbers["green"] = cubeColorNumber
+                        }
+                    }
+                    "blue" -> {
+                        if (cubeColorNumber > fewestCubeNumbers["blue"]!!) {
+                            fewestCubeNumbers["blue"] = cubeColorNumber
+                        }
+                    }
+                    else -> {
+                        println("Other Cube Color")
+                        break
+                    }
+                }
+            }
+        }
+
+        return fewestCubeNumbers
+    }
+
     fun part1(input: List<String>): Int {
         var ret = 0
         for (line in input) {
@@ -65,6 +104,12 @@ fun main() {
         var ret = 0
         for (line in input) {
             println(line)
+            val gameID = getGameID(line)
+            val fewestCubeNumbers = findFewestCubeNumbers(line)
+            fewestCubeNumbers.println()
+            val power = fewestCubeNumbers.values.reduce { acc, i -> acc * i }
+            power.println()
+            ret += power
         }
         println(ret)
         return ret
@@ -73,10 +118,10 @@ fun main() {
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day02_test")
 //    part1(testInput)
-    check(part1(testInput) == 8)
-//    check(part2(testInput) == 2286)
+//    check(part1(testInput) == 8)
+    check(part2(testInput) == 2286)
 
     val input = readInput("Day02")
-    part1(input).println()
-//    part2(input).println()
+//    part1(input).println()
+    part2(input).println()
 }
