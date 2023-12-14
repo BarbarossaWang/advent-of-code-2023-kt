a6499805a10cfc3e9a3017ab537ebb100971a72c
amll <jiewangcn@outlook.com>
2023-12-14 11:27:13 +0800

    Day 2 - Part One

---
+++ src/Day02.kt
@@ -0,0 +1,82 @@
+fun main() {
+    fun getGameID(line: String): Int {
+        val splitList = line.split(":")
+        val gameID = splitList[0].removePrefix("Game ").trim()
+        return gameID.toInt()
+    }
+
+    fun isGamePossible(line: String): Boolean {
+        val cubeNumbersLimit = mapOf("red" to 12, "green" to 13, "blue" to 14)
+        var isPossible = true
+
+        val splitList = line.split(":")
+        val cubeSets = splitList[1].split(";")
+        for (cubeSet in cubeSets) {
+            val cubeNumberColornames = cubeSet.split(",")
+            for (cubeNumberColorname in cubeNumberColornames) {
+                val splitCubeNumberColorname = cubeNumberColorname.trim().split(" ")
+                val cubeColorNumber = splitCubeNumberColorname[0].toInt()
+                val cubeColorName = splitCubeNumberColorname[1]
+
+                when (cubeColorName) {
+                    "red" -> {
+                        if (cubeColorNumber > cubeNumbersLimit["red"]!!) {
+                            isPossible = false
+                            break
+                        }
+                    }
+                    "green" -> {
+                        if (cubeColorNumber > cubeNumbersLimit["green"]!!) {
+                            isPossible = false
+                            break
+                        }
+                    }
+                    "blue" -> {
+                        if (cubeColorNumber > cubeNumbersLimit["blue"]!!) {
+                            isPossible = false
+                            break
+                        }
+                    }
+                    else -> {
+                        println("Other Cube Color")
+                        break
+                    }
+                }
+            }
+        }
+
+        isPossible.println()
+        return isPossible
+    }
+
+    fun part1(input: List<String>): Int {
+        var ret = 0
+        for (line in input) {
+            println(line)
+            val gameID = getGameID(line)
+//            println(gameID)
+            if(isGamePossible(line)) ret += gameID
+        }
+        println(ret)
+        return ret
+    }
+
+    fun part2(input: List<String>): Int {
+        var ret = 0
+        for (line in input) {
+            println(line)
+        }
+        println(ret)
+        return ret
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day02_test")
+//    part1(testInput)
+    check(part1(testInput) == 8)
+//    check(part2(testInput) == 2286)
+
+    val input = readInput("Day02")
+    part1(input).println()
+//    part2(input).println()
+}
